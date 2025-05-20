415b0052bfeb5d6042b5f1c77359dc9c214e97ca
yoni7878 <89324923+yoni7878@users.noreply.github.com>
2025-05-19 16:08:33 -0800

    Update Unknown.lua

--- LevelAPI/Modules/Unknown.lua
+++ LevelAPI/Modules/Unknown.lua
@@ -1,1 +1,7 @@
+local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
 
+Fluent:Notify({
+    Title = "Unknown game, sorry.",
+    Content = "You might've stumbled on slap royale's matchmaking!",
+    Duration = 3
+})
